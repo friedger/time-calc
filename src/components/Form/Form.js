e5c08e0e4b271c6f26acbe18c94d6f283540d2a5
Friedger <friedger@gmail.com>
2018-05-10 07:59:27 +0200

    remove constraint for end date

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -15,8 +15,7 @@ import './Form.css'
 export const SCHEMA = {
   type: 'object',
   required: [
-    'start',
-    'end'
+    'start'
   ],
   properties: {
     start: {type: 'string', pattern: /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/},
