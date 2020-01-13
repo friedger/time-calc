bcdab5c54145de6f9ec12d145a02d5772036bd8b
Friedger Müffke <mail@friedger.de>
2020-01-13 17:15:30 +0100

    remove google fonts, load user profiles async

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,62 +1,57 @@
-import React from 'react';
-import BlockstackSignInButton from './BlockstackSignInButton'
+import React, { useState } from "react";
+import BlockstackSignInButton from "./BlockstackSignInButton";
 
-import { connect } from 'react-redux'
-import { userSignIn, userSignOut} from '../../logic/actions/actions'
+import { connect } from "react-redux";
+import { userSignIn, userSignOut } from "../../logic/actions/actions";
 
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core";
 
 const styles = () => ({
   root: {
-    textAlign:"center"
+    textAlign: "center"
   },
   avatar: {
-    borderRadius:"50%",
-    height:"20px",
-    width:"20px",
+    borderRadius: "50%",
+    height: "20px",
+    width: "20px"
   }
-})
+});
 
-const UserProfile = (props) => {
-  const {
-    isSignedIn,
-    isConnecting,
-    name,
-    avatarUrl,
-    message,
-    classes
-  } = props;
-
+const UserProfile = props => {
+  const { isSignedIn, isConnecting, name, avatarUrl, message, classes } = props;
+  const [imgSrc, setImgSrc] = useState("/blockstack.png");
   if (isSignedIn) {
-    const image = (<img src={avatarUrl} alt={name} className={classes.avatar}/>)
+    const image = <img src={imgSrc} alt={name} className={classes.avatar} />;
+    fetch(avatarUrl)
+      .then(r => r.blob())
+      .then(content => {
+        const imgData = URL.createObjectURL(content);
+        setImgSrc(imgData);
+      });
     return (
-      <div className={classes.root}>          
-      <BlockstackSignInButton
-        signIn = {props.userSignIn}
-        signOut = {props.userSignOut}
-        isSignedIn = {props.isSignedIn}
-        img = {image}
-        includeBlockstackLogo = {false}
-      />
-      </div>
-    )
-  }
-  if (isConnecting) {
-    return (
-      <div>
-      {message}
+      <div className={classes.root}>
+        <BlockstackSignInButton
+          signIn={props.userSignIn}
+          signOut={props.userSignOut}
+          isSignedIn={props.isSignedIn}
+          img={image}
+          includeBlockstackLogo={false}
+        />
       </div>
     );
   }
+  if (isConnecting) {
+    return <div>{message}</div>;
+  }
   return (
     <BlockstackSignInButton
-      signIn = {props.userSignIn}
-      signOut = {props.userSignOut}
-      isSignedIn = {props.isSignedIn}
+      signIn={props.userSignIn}
+      signOut={props.userSignOut}
+      isSignedIn={props.isSignedIn}
     />
   );
-}
+};
 
 UserProfile.propTypes = {
   isSignedIn: PropTypes.bool,
@@ -67,28 +62,32 @@ UserProfile.propTypes = {
   userSignIn: PropTypes.func,
   userSignOut: PropTypes.func,
   classes: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const user = state.userProfile.user;
   const profile = user != null ? state.userProfile.user.profile : null;
   return {
     isSignedIn: user != null,
-    isConnecting: user == null && state.userProfile.userMessage === 'Connecting',
+    isConnecting:
+      user == null && state.userProfile.userMessage === "Connecting",
     name: profile != null ? profile.name : null,
-    avatarUrl: profile != null && "image" in profile && profile.image.length > 0 ? profile.image[0].contentUrl : null,
-    message:  state.userProfile.userMessage
-  }
-}
+    avatarUrl:
+      profile != null && "image" in profile && profile.image.length > 0
+        ? profile.image[0].contentUrl
+        : null,
+    message: state.userProfile.userMessage
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     userSignIn: () => dispatch(userSignIn()),
     userSignOut: () => dispatch(userSignOut())
-    }
-}
+  };
+};
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withStyles(styles) (UserProfile));
+)(withStyles(styles)(UserProfile));
